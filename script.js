7cfaf0a8f4a2472b933e16f47c94e99d014585d4
Jørn Lemika <jorn@lemika.net>
2020-09-23 14:13:19 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+// Question 1
+
+var firstVar;
+
+// Question 2
+
+var name = "Jørn";
+
+// Question 3
+
+var number = 32;
+
+// Question 4
+
+var division = 20/5;
+
+// Question 5
+
+var type = typeof "frog";
+
+// Question 6
+
+var orderHasShipped = false;
+
+// Question 7
+
+if (orderHasShipped === true) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+if (orderHasShipped) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+orderHasShipped ? console.log("true") : console.log("false");
+
+// Question 8
+
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
